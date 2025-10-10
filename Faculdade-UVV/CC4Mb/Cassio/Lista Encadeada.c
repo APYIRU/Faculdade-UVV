eb9792b6410c1af37f6dd0230501614c210ead0e
APYIRU <159280569+APYIRU@users.noreply.github.com>
2025-10-10 10:43:06 -0300

    Update Lista Encadeada.c

--- Faculdade-UVV/CC4Mb/Cassio/Lista Encadeada.c
+++ Faculdade-UVV/CC4Mb/Cassio/Lista Encadeada.c
@@ -60,3 +60,101 @@ int main()
     
     
 }
+
+
+Buscar + Imprimir:
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct cel{
+    int conteudo;
+    struct cel *seg;
+    
+} cel;
+typedef struct cel* Lista;
+
+Lista* cria_lista(){
+    Lista *li = (Lista*) malloc (sizeof(Lista));
+    if(li != NULL){
+        *li = NULL;
+    }
+    return li;
+}
+
+int Buscar(Lista *lista, int x){
+    if(lista == NULL){ return 0; }
+    cel *temp;
+    temp = *lista;
+    while(temp != NULL){
+        if(temp->conteudo == x){
+            printf("%d", x);
+            return x;
+        }
+        temp = temp->seg;
+    }
+    printf("não ta na lista");
+}
+
+int imprimir(Lista* lista){
+    if (lista==NULL){
+        printf("Lista não exite");
+    }
+    cel *temp;
+    temp = *lista;
+    
+    while(temp != NULL){
+        printf("\t%d", temp->conteudo);
+        temp = temp->seg;
+    }
+}
+
+
+
+int insere_lista_fim(Lista *lista, int x){
+    if(lista == NULL){ return 0; }
+    cel* aux = (cel*) malloc(sizeof(cel));
+    if(aux == NULL){ return 0; }
+    aux -> conteudo = x;
+    aux -> seg = NULL;
+    if((*lista) == NULL) { //estou inserindo o primeiro elemento
+    *lista = aux;
+    } else {
+        cel *temp;
+        temp = *lista;
+        while(temp->seg != NULL){
+            temp = temp->seg;
+        }
+        temp->seg = aux;
+    }
+    return 1;
+}
+
+int main()
+{
+    Lista *lst;
+    lst = cria_lista();
+    
+    cel c1, c2, c3;
+    c1.conteudo = 1;
+    c2.conteudo = 2;
+    c3.conteudo = 3;
+    c1.seg = &c2;
+    c2.seg = &c3;
+    c3.seg = NULL;
+    
+    *lst = &c1;
+    
+    cel* temp = *lst;
+    
+    insere_lista_fim(lst, 10);
+    Buscar(lst, 0);
+    
+    while(temp != NULL){
+        printf("\t%d", temp->conteudo);
+        temp = temp->seg;
+    }
+    
+    imprimir(lst);
+    
+    
+}
