43ef18352ff5aed1d26b8d1f8703dd0c53b37b3f
APYIRU <159280569+APYIRU@users.noreply.github.com>
2026-02-05 08:53:26 -0300

    Update Ponto no Circulo.c

--- Faculdade-UVV/CC5Mb/Estrutura de Dados 2/Ponto no Circulo.c
+++ Faculdade-UVV/CC5Mb/Estrutura de Dados 2/Ponto no Circulo.c
@@ -31,3 +31,41 @@ int main()
 
     return 0;
 }
+
+
+EVOLUÇÃO
+
+#include <stdio.h>
+#include <math.h>
+
+typedef struct Circ{
+    float raio, a, b;
+}Circ;
+
+void Circulo(Circ c){
+    int x = 5, y = 6;
+    double d = sqrt(pow((x - c.a), 2) + pow((y - c.b), 2));
+    if(d > c.raio)
+    {
+        printf("Ta fora do circulo");
+    } else if (d < c.raio)
+    {
+        printf("Ta dentro do circulo");
+    } else
+    {
+        printf("Ta igual amigão");
+    }
+}
+
+int main()
+{ 
+    Circ c;
+    c.a = 2;
+    c.b = 2;
+    c.raio = 5;
+    Circulo(c);
+
+
+    return 0;
+}
+
