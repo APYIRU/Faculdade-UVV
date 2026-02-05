4a4aef0069ef244a9ad644743fb8b024887df18d
APYIRU <159280569+APYIRU@users.noreply.github.com>
2026-02-05 07:28:44 -0300

    Add files via upload

---
+++ Faculdade-UVV/CC5Mb/Estrutura de Dados 2/Ponto no Circulo.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct Circ{
+    float raio, a, b;
+}Circ;
+
+void Circulo(Circ c){
+    int x = 4, y = 4;
+    double d = sqrt(pow((x - c.a), 2) + pow((y - c.b), 2));
+    if(d > c.raio)
+    {
+        printf("Ta fora do circulo");
+    } else if (d < c.raio)
+    {
+        printf("Ta dentro do circulo");
+    } else
+    {
+        printf("Ta igual amigão");
+    }
+}
+
+int main()
+{ 
+    Circ c;
+    c.a = 2;
+    c.b = 2;
+    c.raio = 5;
+    Circulo(c);
+
+
+    return 0;
+}
